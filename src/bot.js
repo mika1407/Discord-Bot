c34689bc4c13aec871d3e162b12cc5000e5f5e0a
mika1407 <koneautomaatio@hotmail.com>
2020-11-30 18:12:40 +0200

    guild

--- src/bot.js
+++ src/bot.js
@@ -2,15 +2,25 @@ require('dotenv').config();
 
 const { Client } = require('discord.js');
 const client = new Client();
+const PREFIX = "$"
 
 client.on('ready', () => {
   console.log(`${client.user.tag} has logged in.`)
 })
 
 client.on('message', (message) => {
-  console.log(`[${message.author.tag}]: ${message.content}` )
-  if (message.content === 'hello') {
-    message.reply('hello there!')
+  if (message.author.bot) return
+  if (message.content.startsWith(PREFIX)) {
+    const [CMD_NAME, ...args] = message.content
+      .trim()
+      .substring(PREFIX.length)
+      .split(/\s+/)
+    if (CMD_NAME === 'kick') {
+      if (args.length === 0) return message.reply('Please provide an ID')
+      const member = message.guild.members.cache.get(args[0])
+
+      console.log(member)
+    }
   }
 })
 
