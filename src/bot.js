203a00b645399e2bc726be67e975610caf6f529e
mika1407 <koneautomaatio@hotmail.com>
2020-11-30 13:20:13 +0200

    first commit

---
+++ src/bot.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const { Client } = require('discord.js');
+const client = new Client();
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} has logged in.`)
+})
+
+client.on('message', (message) => {
+  console.log(`[${message.author.tag}]: ${message.content}` )
+  if (message.content === 'hello') {
+    message.reply('hello there!')
+  }
+})
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
